a6091763f2c9bca239f084ec937338b2489e8ee5
bensboto <bensboto@gmail.com>
2023-01-25 15:23:10 -0700

    added setStyles function to add the correct past,present or future class to each time block

--- assets/script.js
+++ assets/script.js
@@ -25,12 +25,13 @@ $(function () {
 let saveButton = $(".saveBtn");
 let container = $(".container-lg");
 let allTimeBlocks = container.children("div");
+let currentDiv = $(allTimeBlocks).get(2);
+console.log(currentDiv);
 
 saveButton.on("click", function () {
   let clickedSaveButton = $(this);
   let textDescription = clickedSaveButton.siblings(".description");
   let currentEl = clickedSaveButton.closest("div");
-
   console.log(allTimeBlocks);
   let index = currentEl.index();
   console.log(index);
@@ -38,24 +39,41 @@ saveButton.on("click", function () {
     let divId = `#hour-` + 9;
     console.log(divId);
     localStorage.setItem(divId, textDescription.val());
-    
   } else {
     divId = `#hour-` + (9 + index);
     localStorage.setItem(divId, textDescription.val());
     console.log(divId);
-
   }
 });
 
-function renderMessage(){
-  for(let i =9; i < 18;i++){
-    let storedData = localStorage.getItem("#hour-"+i)
-    $("#hour-"+i).children("textarea").val(storedData)
+function renderMessage() {
+  for (let i = 9; i < 18; i++) {
+    let storedData = localStorage.getItem("#hour-" + i);
+    $("#hour-" + i)
+      .children("textarea")
+      .val(storedData);
+  }
 }
+
+function setStyle() {
+  let hour = dayjs().hour();
+  for (let i = 0; i < allTimeBlocks.length; i++) {
+    let currentDiv = $(allTimeBlocks.get(i));
+    let currentDivAttr = currentDiv.attr("id").slice(5);
+    console.log(`currentDivId is ${currentDivAttr}`);
+    if (currentDivAttr == hour) {
+      currentDiv.addClass("present");
+    } else if (currentDivAttr < hour) {
+      currentDiv.addClass("past");
+    } else if (currentDivAttr > hour) {
+      currentDiv.addClass("future");
+    }
+  }
 }
 
 function init() {
   renderMessage();
+  setStyle();
 }
 
 init();
@@ -63,3 +81,6 @@ init();
 //adds current day to header space
 let today = dayjs();
 $("#currentDay").text(today.format("MMM D, YYYY"));
+
+let hour = dayjs().hour();
+console.log(`hour is ${hour}`); // gets current hour
