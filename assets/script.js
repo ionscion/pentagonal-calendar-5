1c08c6e8c1b4c87a52f9c0f4e83a485ec5765996
bensboto <bensboto@gmail.com>
2023-01-26 20:37:45 -0700

    cleaned up code and removed most of the console logs

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
 let saveButton = $(".saveBtn");
 let container = $(".container-lg");
 let allTimeBlocks = container.children("div");
-let currentDiv = $(allTimeBlocks).get(2);
-console.log(currentDiv);
 let today = dayjs();
 $("#currentDay").text(today.format("dddd, MMM D, YYYY"));
 let hour = dayjs().hour();
@@ -13,17 +11,13 @@ $(function () {
     let clickedSaveButton = $(this);
     let textDescription = clickedSaveButton.siblings(".description");
     let currentEl = clickedSaveButton.closest("div");
-    console.log(allTimeBlocks);
     let index = currentEl.index();
-    console.log(index);
     if (index === 0) {
       let divId = `#hour-` + 9;
-      console.log(divId);
       localStorage.setItem(divId, textDescription.val());
     } else {
       divId = `#hour-` + (9 + index);
       localStorage.setItem(divId, textDescription.val());
-      console.log(divId);
     }
   });
   function renderMessage() {
@@ -40,7 +34,6 @@ $(function () {
     for (let i = 0; i < allTimeBlocks.length; i++) {
       let currentDiv = $(allTimeBlocks.get(i));
       let currentDivAttr = currentDiv.attr("id").slice(5);
-      console.log(`currentDivId is ${currentDivAttr}`);
       if (currentDivAttr == hour) {
         currentDiv.addClass("present");
       } else if (currentDivAttr < hour) {
